2963746b43f15d57ac724ed246c5092aa950c53e
SoftFever <softfeverever@gmail.com>
2022-11-21 23:04:12 +0800

    wip

--- src/slic3r/GUI/Tab.hpp
+++ src/slic3r/GUI/Tab.hpp
@@ -578,7 +578,6 @@ public:
 	void		extruders_count_changed(size_t extruders_count);
 	PageShp		build_kinematics_page();
 	void		build_unregular_pages(bool from_initial_build = false);
-	void		build_unregular_pages2(bool from_initial_build = false);
 	void		on_preset_loaded() override;
 	void		init_options_list() override;
 	void		msw_rescale() override;
