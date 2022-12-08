05b9ec300d55a283500edf22aa84a0fc5700193b
Shane Warner <shane@3dlabs.io>
2022-12-08 10:36:42 -0500

    more profile tweaking and reorg More renaming work

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -356,7 +356,7 @@ void Tab::create_preset_tab()
     m_top_sizer->Add( m_search_item, 1, wxALIGN_CENTER_VERTICAL | wxRIGHT , 8 );
 
     if (dynamic_cast<TabPrint*>(this) == nullptr) {
-        m_static_title = new Label(m_top_panel, Label::Body_12, _L("Advance"));
+        m_static_title = new Label(m_top_panel, Label::Body_12, _L("Expert"));
         m_static_title->Wrap( -1 );
         // BBS: open this tab by select first
         m_static_title->Bind(wxEVT_LEFT_UP, [this](auto& e) {
