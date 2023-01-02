f7b67e5d7f8616d66c2bf31580ce8dbb7fd22f15
Shane Warner <shane@3dlabs.io>
2023-01-01 23:29:40 -0500

    change verbage on calibration items

--- src/slic3r/GUI/MainFrame.cpp
+++ src/slic3r/GUI/MainFrame.cpp
@@ -2477,16 +2477,16 @@ void MainFrame::init_menubar_as_editor()
 
     // PA
     auto pa_menu = new wxMenu();
-    append_menu_item(pa_menu, wxID_ANY, _L("Line method - DDE"), _L(""),
+    append_menu_item(pa_menu, wxID_ANY, _L("Line method"), _L(""),
         [this](wxCommandEvent&) { if (m_plater) m_plater->calib_pa(true, false); }, "", nullptr,
         [this]() {return m_plater->is_view3D_shown();; }, this);
-    append_menu_item(pa_menu, wxID_ANY, _L("Tower method - DDE"), _L(""),
+    append_menu_item(pa_menu, wxID_ANY, _L("Tower method"), _L(""),
         [this](wxCommandEvent&) { if (m_plater) m_plater->calib_pa(false, false); }, "", nullptr,
         [this]() {return m_plater->is_view3D_shown();; }, this);
     m_topbar->GetCalibMenu()->AppendSubMenu(pa_menu, _L("Pressure Advance"));
 
     // help 
-    append_menu_item(m_topbar->GetCalibMenu(), wxID_ANY, _L("Tutorial"), _L("Calibration help"),
+    append_menu_item(m_topbar->GetCalibMenu(), wxID_ANY, _L("Help"), _L("Calibration help"),
         [this](wxCommandEvent&) { wxLaunchDefaultBrowser("https://docs.3dlabs.io/3dlabs-studio/Calibration", wxBROWSER_NEW_WINDOW); }, "", nullptr,
         [this]() {return m_plater->is_view3D_shown();; }, this);
 
