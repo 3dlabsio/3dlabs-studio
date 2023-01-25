48165e586ee604f6458fc35d2b5825f3069f4028
Shane Warner <shane@3dlabs.io>
2023-01-25 00:49:14 -0500

    update wording

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -2188,10 +2188,9 @@ void PrintConfigDef::init_fff_params()
     
     def = this->add("post_process", coStrings);
     def->label = L("Post-processing Scripts");
-    def->tooltip = L("If you want to process the output G-code through custom scripts, "
-                   "just list their absolute paths here. Separate multiple scripts with a semicolon. "
+    def->tooltip = L("Add G-Code post processing scripts here. Multiple scripts can be run - separate each one with a semicolon."
                    "Scripts will be passed the absolute path to the G-code file as the first argument, "
-                   "and they can access the Slic3r config settings by reading environment variables.");
+                   "and they can access the slicer config settings by reading environment variables.");
     def->gui_flags = "serialized";
     def->multiline = true;
     def->full_width = true;
